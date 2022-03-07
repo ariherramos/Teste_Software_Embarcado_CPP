efd7cfc760469f5bb6d102eed298346b9205af9e
Arihé Redivo Ramos <ariherramos@gmail.com>
2022-03-07 04:19:08 -0300

    cmoka

---
+++ cmocka-unit-test-setup-teardown/src/soma.c
@@ -0,0 +1,6 @@
+#include "soma.h"
+
+int soma(int a, int b)
+{
+    return a + b;
+}
---
+++ cmocka-unit-test-setup-teardown/src/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "soma.h"
+
+int main(void)
+{
+    printf("Soma(10, 15)=%d\n", soma(10, 15));
+}
