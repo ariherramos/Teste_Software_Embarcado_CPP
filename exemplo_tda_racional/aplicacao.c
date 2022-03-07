8645c4354b931ed191776bba36cdc251a6156aaf
Arihé Redivo Ramos <ariherramos@gmail.com>
2022-03-07 04:12:38 -0300

    Estudo de testes em software embarcados

---
+++ exemplo_tda_racional/aplicacao.c
@@ -0,0 +1,23 @@
+#include "aplicacao.h"
+#include <stdio.h>
+
+int main(void)
+{
+	racional_t *a, *b, *c;
+	a = cria_racional(3, 5);
+	b = cria_racional(6, 3);
+	c = soma_racional(a, b);
+	mostra_racional(a);
+	printf(" + ");
+	mostra_racional(b);
+	printf(" = ");
+	mostra_racional(c);
+	printf("\n");
+	destroi_racional(&a);
+	printf("Endereco de a = %p\n", a);
+	
+	//a->dem = 10;
+	
+	destroi_racional(&b);
+	destroi_racional(&c);
+}
